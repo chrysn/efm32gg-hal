2be8625057a9b3d8c9d13094e3b0afa7ed4065f4
Nurahmadie <nurahmadie@gmail.com>
2019-02-06 08:29:30 +0700

    WIP Encode pin states as type for GPIOs

--- src/gpio/common.rs
+++ src/gpio/common.rs
@@ -2,9 +2,91 @@ use super::Pins;
 use crate::cmu;
 use embedded_hal::digital;
 
-pub struct Disabled;
-pub struct Output;
-pub struct Input;
+/// State type for pin with disabled input.
+/// In disabled state output pin can be in either
+/// floating state (dout 0), or pulled up (dout 1)
+pub struct Disabled<TYPE> {
+    _private: PhantomData<TYPE>,
+}
+
+/// In efr32 chip series, casting pin into Output will set DINDIS register to 1.
+/// There is no Output to Input conversion in any direction.
+/// To enable both Input and Output, cast to `Input<Output<_state_>>` type from Disabled.
+/// See Input type below.
+/// List of possible Output mode:
+///     Output<PushPull<Normal>>
+///     Output<PushPull<Alternate>>
+///     Output<WiredOr<Floating>>
+///     Output<WiredOr<PullDown>>
+///     Output<WiredAnd<Normal, Floating>>
+///     Output<WiredAnd<Normal, PullUp>>
+///     Output<WiredAndFilter<Normal, Floating>>
+///     Output<WiredAndFilter<Normal, PullUp>>
+///     Output<WiredAnd<Alternate, Floating>>
+///     Output<WiredAnd<Alternate, PullUp>>
+///     Output<WiredAndFilter<Alternate, Floating>>
+///     Output<WiredAndFilter<Alternate, PullUp>>
+pub struct Output<TYPE> {
+    _private: PhantomData<TYPE>,
+}
+
+/// List of possible input mode:
+///     Input<Floating>
+///     Input<PullUp>
+///     Input<PullDown>
+///     Input<WithFilter<Floating>>
+///     Input<WithFilter<PullDown>>
+///     Input<WithFilter<PullUp>>
+///
+/// This mode, will also has Output implemented, see Output type above:
+///     Input<_all output modes_>
+pub struct Input<TYPE> {
+    _private: PhantomData<TYPE>,
+}
+
+/// Pin states.
+pub struct Floating;
+pub struct PullUp;
+pub struct PullDown;
+
+/// Output mode.
+///
+/// PushPull can have:
+///     PushPull<Normal>    // for efr32, Normal config in ctrl also can be configured separately.
+///     PushPull<Alternate> // for efm32, Alternate config only change its current drive mode.
+pub struct PushPull<CFG> {
+    _private: PhantomData<CFG>,
+}
+
+/// OpenSource (wired "or") pin is set to be driven high.
+/// Its state can be Floating or PullDown.
+pub struct OpenSource<STATE> {
+    _private: PhantomData<STATE>,
+}
+
+/// OpenDrain (wired "and") pin is set to be driven low.
+/// Its state can be Floating or PullUp.
+pub struct OpenDrain<STATE> {
+    _private: PhantomData<STATE>,
+}
+
+type WiredOr = OpenSource;
+type WiredAnd = OpenDrain;
+
+pub struct Normal;
+pub struct Alternate;
+
+pub enum DriveMode {
+    High,
+
+    Lowest,
+
+    #[cfg(not(feature = "chip-efr32xg1"))]
+    Standard,
+
+    #[cfg(not(feature = "chip-efr32xg1"))]
+    Low,
+}
 
 pub trait GPIOExt {
     fn split(self, gpioclk: cmu::GPIOClk) -> Pins;
